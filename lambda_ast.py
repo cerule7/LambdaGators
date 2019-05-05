30dc58843e646516f3a046fe04bc51e5bb54670d
Charles Rule <cruleis@gmail.com>
2019-05-05 15:06:08 -0400

    removed debug statements

--- lambda_ast.py
+++ lambda_ast.py
@@ -2,7 +2,6 @@ class Abstraction():
 	def __init__(self, param, body):
 		self.param = param
 		self.body = body
-		print(self.toString())
 
 	def toString(self):
 		return ('λ' + self.param + '.' + self.body.toString())
@@ -11,7 +10,6 @@ class Application():
 	def __init__(self, lhs, rhs):
 		self.rhs = rhs
 		self.lhs = lhs
-		print("APP MADE FOR " + self.rhs.toString() + " & " + self.lhs.toString())
 
 	def toString(self):
 		return "APP" + self.lhs.toString() + " " + self.rhs.toString()
@@ -20,7 +18,6 @@ class Identifier():
 	def __init__(self, value, name):
 		self.value = value
 		self.name = name
-		print("ID MADE FOR " + self.name)
 
 	def toString(self):
 		return self.name
