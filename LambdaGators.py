52dfbd1f37dffcd30f3ce0b68a82ebcf058a0f75
Michael Rizzo <mike@nbp-101-216.nbp.ruw.rutgers.edu>
2019-05-09 18:06:51 -0400

    LambdaGators.py main NOW with render function

--- LambdaGators.py
+++ LambdaGators.py
@@ -1,6 +1,35 @@
 import pygame as pg
 import os
-import renderer as rend
+import random
+
+def randomColor():
+        x = random.randint(10, 240)
+        y = random.randint(10, 240)
+        z = random.randint(10, 240)
+
+        return (x, y, z, 0)
+
+def render(varList):
+        baseColor = (0, 0, 0) #constant value of the base color gator 
+        thresh = (0, 0, 0) #threshold value for pygame transform 
+
+        imgDict = {} #dictionary of variables to reference alligator/func or egg/var of variable specific color
+
+        for var in varList:
+                imgDict[var] = {} #sub-dictionary per variable
+                
+                newColor = randomColor() #new random color for new var
+                
+                tempGator = pg.image.load('Resources/alligator.png') #loads temp Surfaces
+                tempEgg = pg.image.load('Resources/egg.png')
+
+                pg.transform.threshold(tempGator, tempGator, baseColor, thresh, newColor, 1, None, False) #makes Surfaces the new var color
+                pg.transform.threshold(tempEgg, tempEgg, baseColor, thresh, newColor, 1, None, False) 
+
+                imgDict[var]['alligator'] = tempGator #adds gator and egg to var sub-dict 
+                imgDict[var]['egg'] = tempEgg
+ 
+        return imgDict
 
 pg.init()
 screen = pg.display.set_mode((1000, 750))
